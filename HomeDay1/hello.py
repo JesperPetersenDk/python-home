5c607c395bee8b0ac83e1f451ee7ec11ba66eb63
Jesper Petersen <info@ordklar.dk>
2019-09-27 18:57:48 +0200

    update

--- HomeDay1/hello.py
+++ HomeDay1/hello.py
@@ -1,8 +1,20 @@
-print("hello world")
-
-print("Tester")
-
 age = 26
-name = "Jesper P"
+name = "jesper p"
 
+# Velkommen til (navn) din alder er : xxx
 print("Velkommen til " + name + " din alder er : " + str(age))
+
+# xx here
+print(age)
+
+# laver bogstaverne stor..
+print(name.upper())
+
+#Er det decimal tal?
+print(name.isdecimal())
+
+#Er det lower?
+print(name.islower())
+
+#Fortæller hvor mange tegn der er?
+print(len(name))
